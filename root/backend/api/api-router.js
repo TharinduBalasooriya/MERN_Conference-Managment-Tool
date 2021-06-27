05400662f24792f28f83e1b5d5ddeed008e5eff8
tharinduB <tharinduudana66@gmail.com>
2021-06-27 19:52:56 +0530

    Admin Login Finish

--- root/backend/api/api-router.js
+++ root/backend/api/api-router.js
@@ -1,11 +1,20 @@
 let router = require("express").Router();
 
+//Import controller
+let authController =  require('../controller/auth-controller')
+
+
+//Import Middlewares
+let verify =  require('../middleware/VerifySingUP')
+
 router.get("/", (req, res) => {
     res.send("ICAF API Working");
   });
 
 
   //User Handler routes
+  router.route("/users").post(verify.checkDuplicateUsernameOrEmail,authController.signUP)
+  router.route("/auth/login").post(authController.signIn)
 
 
 
--- root/backend/middleware/AuthMiddleware.js
+++ root/backend/middleware/AuthMiddleware.js
@@ -1,2 +1,19 @@
+const jwt =  require("jsonwebtoken");
 
-//Middle ware
+
+let verifyToken =  function(req,res,next){
+
+    let token = req.headers["token"];
+    if(!token){
+        return res.status(403).send({message:"No token available!!!"});
+    }
+    jwt.verify(token,'key',(err,decoded)=>{
+
+        if(err){
+            return res.status(401).send({message:"Unotherized user"});
+        }
+        req.userId = decoded.id;
+        next();
+    })
+}
+module.exports = verifyToken;
