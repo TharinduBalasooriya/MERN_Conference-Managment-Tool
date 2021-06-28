1346d37554224757ffb9dd43f6812139ec990d00
Dinuja-Spinto <sri.prabasha@gmail.com>
2021-06-29 00:27:54 +0530

    Keynote Component

--- root/frontend/src/App.jsx
+++ root/frontend/src/App.jsx
@@ -2,6 +2,10 @@ import React  from "react";
 import { BrowserRouter as  Router,Route,Switch } from "react-router-dom";
 import AdminLogin from './component/adminlogin/login.component'
 import AdminDash from './component/adminDashboard/admin.component'
+import ukeynotes from "./component/keynoteComponent/keynoteUserView";
+import MangKeynote from "./component/keynoteComponent/manageKeynote";
+import NewKeynote from "./component/keynoteComponent/AddKeyNote";
+import EditKeynote from "./component/keynoteComponent/editKeynote";
 import "bootstrap/dist/css/bootstrap.min.css";
 export default class App extends React.Component {
     constructor(props) {
@@ -14,6 +18,10 @@ export default class App extends React.Component {
                     <Switch>
                         <Route exact path="/admin" component={AdminLogin}></Route>
                         <Route  path="/admin-dash" component={AdminDash}></Route>
+                        <Route  path="/u-keynotes" component={ukeynotes}></Route>
+                        <Route  path="/mng-keynotes" component={MangKeynote}></Route>
+                        <Route  path="/new-keynotes" component={NewKeynote}></Route>
+                        <Route  path="/edit-keynotes/:id" component={EditKeynote}></Route>
                     </Switch>
 
 
