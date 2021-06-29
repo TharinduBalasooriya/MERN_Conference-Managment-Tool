86bbe80834eb4aea02934857db6db10b1eebe900
Kulanika <kulanikag2017@gmail.com>
2021-06-29 07:53:13 +0530

    Created HomePage and header

---
+++ root/frontend/src/page/homePage/homePage.component.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+
+class HomePage extends Component{
+    constructor(props){
+        super(props);
+        debugger;
+        this.state = {
+            
+        }
+
+        this.onSubmit = this.onSubmit.bind(this);
+        this.onChange = this.onChange.bind(this);
+    }
+
+
+    render(){
+        return(
+            <div>
+            </div>
+        )
+    };
+}
+
+export default HomePage;
